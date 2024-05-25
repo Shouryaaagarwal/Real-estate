5e9fb101fd404cc172f89a51052758764afa7b11
Shourya <shourya0864@gmail.com>
2024-05-25 15:20:59 +0530

    connect to database

--- Api/index.js
+++ Api/index.js
@@ -1,9 +1,19 @@
-import express from 'express'  ;  
-    // type = "module" 
+import express from "express";
+import mongoose from "mongoose"; 
+import dotenv from "dotenv"  ; 
+dotenv.config() ;
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to database")
+}) 
+.catch((error)=>{
+console.log(error)
+});
+// type = "module"
 // const express  =  require("express")
 
-const app = express() ; 
+const app = express();
 
-app.listen(3000, ()=>{
-    console.log('Server is running on 3000')
-})
+app.listen(3000, () => {
+  console.log("Server is running on 3000");
+});
