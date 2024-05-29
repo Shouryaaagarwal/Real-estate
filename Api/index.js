0b82a1b93f3aaa5a9d42116487e9445af7635a46
Shourya <shourya0864@gmail.com>
2024-05-29 10:54:36 +0530

    create user update api

--- Api/index.js
+++ Api/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose"; 
 import dotenv from "dotenv"  ;  
 import userRouter from "./routes/user.route.js" 
-import authRouter from  "./routes/auth.route.js"
+import authRouter from  "./routes/auth.route.js" 
+import cookieParser from "cookie-parser";
 dotenv.config() ;
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -14,9 +15,9 @@ console.log(error)
 // type = "module"
 // const express  =  require("express")
 
-const app = express();  
+const app = express();   
 app.use(express.json()) ;
-
+app.use(cookieParser()) ;
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)  
 app.use((err, req , res , next )=>{
