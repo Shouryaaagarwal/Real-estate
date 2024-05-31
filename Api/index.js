cef8cc638fd2549201cd4bb249739715682ddd91
Shourya <shourya0864@gmail.com>
2024-05-31 12:22:19 +0530

    create listing api route

--- Api/index.js
+++ Api/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import mongoose from "mongoose"; 
 import dotenv from "dotenv"  ;  
-import userRouter from "./routes/user.route.js" 
-import authRouter from  "./routes/auth.route.js" 
-import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.route.js" ;
+import authRouter from  "./routes/auth.route.js" ;
+import cookieParser from "cookie-parser"; 
+import listingRouter from "./routes/listing.route.js"
 dotenv.config() ;
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -19,7 +20,8 @@ const app = express();
 app.use(express.json()) ;
 app.use(cookieParser()) ;
 app.use("/api/user", userRouter)
-app.use("/api/auth", authRouter)  
+app.use("/api/auth", authRouter)   
+app.use("/api/listing", listingRouter) ;
 app.use((err, req , res , next )=>{
   const statusCode = err.statusCode||500  ;
   const message  = err.message||'Internal Message Error' ;
