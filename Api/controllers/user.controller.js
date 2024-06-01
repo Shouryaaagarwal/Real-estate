d771f4ec5f010cbade79ec2add52ff987e65a553
Shourya <shourya0864@gmail.com>
2024-06-01 22:43:58 +0530

    create get user listings

--- Api/controllers/user.controller.js
+++ Api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.utils.js";
 import bcryptjs from "bcryptjs";
@@ -43,4 +44,20 @@ export const deleteUser =async (req, res, next)=>{
     } catch(error){
       next(error) ;
     }
-} ;
+} ; 
+
+
+export const getUserListings =async(req, res, next)=>{
+  if(req.user.id===req.params.id){
+  try{
+ const listings = await Listing.find({UserRef:req.params.id}) ; 
+ res.status(200).json(listings) ;
+
+  } catch(error){
+    next(error) ;
+  } 
+}
+else{
+  return next(errorHandler(401, 'You can only view your own listings'))
+}
+}
